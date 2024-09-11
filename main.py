34100a10376327c0765610f68f968ef9f87975fc
JimmyKiedisUFGD <leonardo.oliveira071@academico.ufgd.edu.br>
2024-09-11 15:58:22 -0400

    IncludeDef

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import time
 import pygame
+
+def heuristica(board, jogador):     #estima valor do tabuleiro
+
+def jogada(board, pos, jogador):    #computa resultado da jogada
+
+def jogadasPossiveis(board):       #lista de jogadas possiveis
+
+def minimax(board, jogador, eu, maxdepth=9):
+
+def bestAction(board, eu):          #retorna a melhor jogada
