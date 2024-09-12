0b3dfee5904cbdb0a4c087dcacf906180cd75e68
Jimmy Kiedis <jinkiedis@icloud.com>
2024-09-11 23:26:37 -0400

    minimaxProgresso0.1

--- main.py
+++ main.py
@@ -21,19 +21,32 @@ def jogadasPossiveis(board):                    #lista de jogadas possiveis
                 jogadas.append([linha,coluna])  #adiciona a lista
     return jogadas                              #aqui estamos retornando todas posições vazias no tabuileiro para ver todas as jogadas possiveis
 
-def minimax(board, jogador, eu, maxdepth=9):
+def minimax(board, jogador, eu, maxdepth=9):    #minimax é um algoritmo da IA, aqui ela montará seu score
+    jogadas = jogadasPossiveis(board)           #precisa das jogadas possiveis para o minimax poder calcular qual é a melhor
     w = checkWinner(board)                      #w recebe da funcao chegckwinner se alguém ganhou
-    if w == eu:                                 #se a IA vencer ela recebe 1
+    
+    if w == eu:                                 #se a IA vencer ela ganha 1
         return 1                                
     elif w != eu and not isFull(board):         #se a IA perder ela ela perde 1
         return -1
     elif isFull(board):                         #se der empate ela não recebe nada e ne perde nada
         return 0
 
+    if jogador == eu:                           #Max
+        best = -float('inf')                    #vamos percorrer todas as possiblidades a partir do tabuleiro atual e retornar o valor da maior delas
+        for jogada in jogadas:
+            resultado = resultJogada(board, jogadas[jogada], jogador)
+            valor = minimax(resultado, 'O' if jogador == 'X' else 'X', eu)
+            if valor > best:
+                best = valor
+        '''aqui preciso terminar de fazer, deve esta em mais ou menos 30 min de aula e preciso dormir'''
+    else:                                       #Min
+        
 def bestAction(board, eu):                      #retorna a melhor jogada
     jogadas = jogadasPossiveis(board)           #recebe a lista de todas as jogadas possiveis
     best = -float('inf')                        #Inicializar o valor infinito negativo, assim, qualquer valor recebido por ele vai ser maior e assim este será substituido
     bestAct = None                              #inicia a melhor ação sem nenhuma valor
+    
     for jogada in jogadas:                      #percorre todas as jogadas possiveis de jogada em jogada
         resultado = resultJogada(board, jogadas[jogada], eu)    #chama a computação de resultados e passa como parametro o tabuleiro, a lista de jogadas possiveis e se o jogador é x ou o
         valor = minimax(board, eu, eu)          #valore drecebera o resultado de minimax que recebe como parametro onde o X ou O foi jogada
